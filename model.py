f8e22593788ded6d2986efbd48bdac73e9a5cf7f
root <root@AshwinLaptop>
2024-06-26 09:03:35 -0500

    before commit

--- model.py
+++ model.py
@@ -31,52 +31,12 @@ class model(nn.Module):
 
     def createVisualModel(self):
         #self.visualModel = nn.Sequential(nn.Flatten(), nn.Linear(112*112, 512), nn.ReLU(), nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 128))
-        #self.visualModel = nn.Sequential(nn.Conv2d(3, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32), nn.Conv2d(3, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32),nn.Conv2d(3, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32), nn.MaxPool2d(2,(2,2)), nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,2)), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,2)), nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128), nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128),nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128), nn.Conv2d(64,128,3))
-        self.visualModel = nn.Sequential(
-    nn.Conv2d(3, 32, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(32),
-    nn.Conv2d(32, 32, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(32),
-    nn.Conv2d(32, 32, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(32),
-    nn.MaxPool2d(2, 2),
-    nn.Conv2d(32, 64, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.Conv2d(64, 64, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.Conv2d(64, 64, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.MaxPool2d(2, 2),
-    nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.MaxPool2d(2, 2),
-    nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 3, padding=1)
-)
-
+        self.visualModel = nn.Sequential(nn.Conv2d(3, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32), nn.Conv2d(3, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32),nn.Conv2d(3, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32), nn.MaxPool2d(2,(2,2)), nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,2)), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,2)), nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128), nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128),nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128), nn.Conv2d(64,128,3))
 
     def createAudioModel(self):
         #self.audioModel = nn.Sequential(nn.Flatten(), nn.Linear(299*13, 512), nn.ReLU(), nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 128))
-        #self.audioModel = nn.Sequential(nn.Conv2d(1, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32), nn.Conv2d(1, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32),nn.Conv2d(1, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32), nn.MaxPool2d(2,(2,1)), nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,1)), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,1)), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,2)), nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128), nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128),nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128), nn.Conv2d(64,128,3))
-        self.audioModel = nn.Sequential(
-    nn.Conv2d(1, 32, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(32),
-    nn.Conv2d(32, 32, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(32),
-    nn.Conv2d(32, 32, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(32),
-    nn.MaxPool2d((2, 1)),
-    nn.Conv2d(32, 64, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.Conv2d(64, 64, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.Conv2d(64, 64, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.MaxPool2d((2, 1)),
-    nn.Conv2d(64, 64, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.Conv2d(64, 64, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.Conv2d(64, 64, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
-    nn.MaxPool2d((2, 1)),
-    nn.Conv2d(64, 128, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.MaxPool2d((2, 2)),
-    nn.Conv2d(128, 128, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 1, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
-    nn.Conv2d(128, 128, 1, padding=1)
-)
+        self.audioModel = nn.Sequential(nn.Conv2d(1, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32), nn.Conv2d(1, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32),nn.Conv2d(1, 32 , 3), nn.ReLU(), nn.BatchNorm2d(32), nn.MaxPool2d(2,(2,1)), nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(32, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,1)), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,1)), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64),nn.Conv2d(64, 64 , 3), nn.ReLU(), nn.BatchNorm2d(64), nn.MaxPool2d(2,(2,2)), nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128), nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128),nn.Conv2d(64, 128 , 3), nn.ReLU(), nn.BatchNorm2d(128), nn.Conv2d(64,128,3))
+
 
 
 
